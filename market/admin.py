c116294ce2821c3953f58d9a450b3e0867e25c5a
Unknown <Oksak208@gmail.com>
2019-01-17 12:38:56 +0300

    hz

--- market/admin.py
+++ market/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Items)
 admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Atribute)
+admin.site.register(ItemAttribute)
--- market/models.py
+++ market/models.py
@@ -10,10 +10,35 @@ class Items(models.Model):
     rate = models.DecimalField(max_digits=2, decimal_places=2)
     count = models.IntegerField(default=0)
     date = models.DateField(auto_now_add=True)
-    category = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    folder = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemAttribute(models.Model):
+    item = models.ForeignKey('Items', on_delete=models.SET_NULL, null=True)
+    attr = models.ForeignKey('Atribute', on_delete=models.SET_NULL, null=True)
+    value = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.item) + " - " + str(self.attr)
+
+
+class Atribute(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.category) + " - " + str(self.name)
 
 
 class Image(models.Model):
