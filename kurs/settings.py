a3dfb1108bb4fad96a759bdbbe2a30c6e8fa31c4
timur <oksak208@gmail.com>
2019-03-16 19:30:38 +0300

    - remove CKeditor in settings for test deploy

--- kurs/settings.py
+++ kurs/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
+    # 'ckeditor',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
